60a46e6f4e3ea9907f16358630e747bc4e485b8f
knstntnn <59287703+knstntnn@users.noreply.github.com>
2023-05-01 17:42:04 +0800

    update script
    update script

--- usr/bin/ftxtx.sh
+++ usr/bin/ftxtx.sh
@@ -1,8 +1,39 @@
 #!/bin/sh
 # ftxtx.sh  --knn_knstntnn 200922   
+# 230126-edit
+
+########################
+#  LAST:
+# --потом проб-ь прикрутить - 'yad' ( ~= + функции)
+# 
+#    --no-cancel 
+#  'NoChacel-button' (  +/- задание в cfg)
+#  $noCancel="--no-cancel"
+# -- noCancel= - awk-ать из CFG !
+#    в кфг -  пустое 'noCancel=' / ?  и + потом - для yad
+#   
+# ?? добавить 'yad' ( на-выбор /?выбор из конфига, а )
+# 
+# --нужно "удобнее подтверждение по клавише"(в окне-Диалога) 
 
 
-DIALOG=Xdialog 
+
+####>> vars :
+confDir=$HOME/.config/ftxtx
+confFile=$confDir/ftxtx.conf
+gtkrcFile=$confDir/ftxtx.Xd.rc
+ftxtxDir=$HOME/ftxtx
+
+
+####>> mk user-cfgs : 
+[[ -d $confDir ]] || ` mkdir $confDir ;  cp /etc/ftxtx/* $confDir ;  echo "ftxtxDir=$ftxtxDir" >> $confFile ; echo "gtkrcFile=$gtkrcFile" >> $confFile `
+
+
+####>> ?? +/- ?
+[[ -f $confFile ]] || ` echo >> $confFile ; echo "ftxtxDir=$ftxtxDir" >> $confFile `
+ 
+
+
 
 fileDate=$(date +%F_%H-%M-%S)
 
@@ -14,8 +45,19 @@ echo -e "$t" >> /tmp/ftxtx-$fileDate.txt
 tChk=$(sed -n '1,12p' < /tmp/ftxtx-$fileDate.txt | cut -c-100 )
 
 IFS=$'\n'
+ 
 
-name=`$DIALOG   --stdout --title "ftxtx: вв. начало имени ..-$fileDate.txt"  --inputbox "часть содержания буфера:\n \n $tChk " 30 100 `
+## --rc-file $gtkrcFile
+## 
+## $gtkrcFile - из $confFile
+## 
+
+########################
+
+DIALOG=Xdialog 
+
+name=`$DIALOG   --icon /usr/share/pixmaps/ftxtx.xpm --rc-file $gtkrcFile   --no-cancel --stdout  --title "ftxtx: вв. начало имени ..-$fileDate.txt"  --inputbox  "Часть содержания буфера:\n \n $tChk " 30 100 `
+
 
 case $? in
       0)
@@ -26,13 +68,68 @@ case $? in
 	echo "Нажата клавиша ESC." ;  rm /tmp/ftxtx-$fileDate.txt  ; exit 0;; 
  esac
 
-mv /tmp/ftxtx-$fileDate.txt $HOME/$name-$fileDate.txt
+ 
 
-IFS=$' '
+ftxtxDir=$(echo `awk -F 'ftxtxDir=' ' { print $2 }  ' $confFile`)
 
-txtViewer=$(echo `awk -F 'txtViewer=' ' { print $2 }  ' $HOME/.config/ftxtx.conf`) 
+[[ -d $ftxtxDir ]] || mkdir  $ftxtxDir
 
-$txtViewer "$(echo $HOME/$name-$fileDate.txt)" 
+
+
+####>> chk :
+[[  -z "$name" ]] && sffx=_$fileDate
+[[ -f $ftxtxDir/${name// /_}.txt ]] && sffx=_$fileDate
+
+
+## mv /tmp/ftxtx-$fileDate.txt $ftxtxDir/$name$sffx.txt
+
+####>> rplc " " --> "_"
+file=$ftxtxDir/${name// /_}$sffx.txt
+
+
+####>> +  name,path
+echo -e "${name}\n"'file://'"${file}\n""=========\n""=========\n""$(cat /tmp/ftxtx-$fileDate.txt)" > /tmp/ftxtx-$fileDate.txt
+
+
+####>> + 4-empty
+echo -e "\n\n\n""=========\n""=========\n" >> /tmp/ftxtx-$fileDate.txt
+
+
+## !! ?? 'time' echo--vs--tee (https://linuxize.com/post/bash-append-to-file/)
+
+
+## ## ## !!!
+##  mv /tmp/ftxtx-$fileDate.txt "${file}" 2> /tmp/ftxtx.err  
+
+[[ -z "$(cat /tmp/ftxtx.err)" ]] ||  ` Xdialog --title "ftxtx:" --msgbox "$(cat /tmp/ftxtx.err)" 30 100 ; exit 1  `
+
+
+
+## ## ## 
+## last comment --/root/ftxtx.sh: line 135: $'\n\nleafpad': command not found
+##   IFS=$' '
+## -- ? или обратку для IFS=$' ' ...или убрать пуст-строки в конфиге !!!.....
+
+txtViewer=$(echo `awk -F 'txtViewer=' ' { print $2 }  ' $confFile`) 
+## 
+## -- ^^ -- в итоге потом нужно:
+## txtViewer=$(echo `awk -F 'txtViewer=' '{ print $2 }' $HOME/.config/ftxtx/ftxtx.conf`) 
+
+
+
+[[ -z "$(cat /tmp/ftxtx.err)" ]] &&  "${txtViewer}" "${file}" 
+
+ rm /tmp/ftxtx.err
+
+
+##   2>&1 read a >  Xdialog     --title "ftxtx:" --msgbox "${a}" 30 100
+
+##  Надо перенаправить поток ошибок на дескриптор стандартного потока вывода: 
+## 
+## 2>&1
+## 
+
+## ? + - если ошибка, то её вывести ? 
 
 exit 0
 
