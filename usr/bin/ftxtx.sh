ba4903e947848ecd166eeaa3219e67b1108e84f1
knstntnn <59287703+knstntnn@users.noreply.github.com>
2024-12-20 01:28:00 +0800

    Add files via upload

--- usr/bin/ftxtx.sh
+++ usr/bin/ftxtx.sh
@@ -1,28 +1,31 @@
-
 #!/bin/sh
 # ftxtx.sh  --knn_knstntnn 230512  
+# --edit 241114: add 'Button xdg-open $ftxtxDir' + some-edit
+# -- ... : $fm , 'clipboard-if-primary==0', 'выводы ошибок'.
 
-
-####>> vars :
+####>> init-vars :
 confDir=$HOME/.config/ftxtx
 confFile=$confDir/ftxtx.conf
 gtkrcFile=$confDir/ftxtx.Xd.rc
 ftxtxDir=$HOME/ftxtx
 
-
 ####>> mk user-cfgs : 
-[[ -d $confDir ]] || ` mkdir $confDir ;  cp /etc/ftxtx/* $confDir ;  echo "ftxtxDir=$ftxtxDir" >> $confFile ; echo "gtkrcFile=$gtkrcFile" >> $confFile `
+[[ -d $confDir ]] || ` mkdir $confDir ; cp /etc/ftxtx/* $confDir ;  echo "ftxtxDir=$ftxtxDir" >> $confFile ; echo "gtkrcFile=$gtkrcFile" >> $confFile `
 
+####>> conf-vars :
+ftxtxDir=$(grep ftxtxDir $confFile | cut -d '=' -f 2)
+gtkrcFile=$(grep gtkrcFile $confFile | cut -d '=' -f 2)
+fm=$(echo `awk -F 'fm=' '{ print $2 }' $confFile`)
 
-####>> ?? +/- ?
-[[ -f $confFile ]] || ` echo >> $confFile ; echo "ftxtxDir=$ftxtxDir" >> $confFile `
+[[ -d $ftxtxDir ]] || mkdir $ftxtxDir
+
+####>> ?? +/- (при случайн. удалении)?
+##[[ -f $confFile ]] || ` echo >> $confFile ; echo "ftxtxDir=$ftxtxDir" >> $confFile `
  
-
-
-
 fileDate=$(date +%F_%H-%M-%S)
 
 t=`xsel -o `
+[ -z "${t}" ] && t=`xsel -ob`
 
 echo "$@" > /tmp/ftxtx-$fileDate.txt
 echo -e "$t" >> /tmp/ftxtx-$fileDate.txt
@@ -31,66 +34,67 @@ tChk=$(sed -n '1,12p' < /tmp/ftxtx-$fileDate.txt | cut -c-100 )
 
 IFS=$'\n'
  
-
-
 DIALOG=Xdialog 
 
-name=`$DIALOG   --icon /usr/share/pixmaps/ftxtx.xpm --rc-file $gtkrcFile   --no-cancel --stdout  --title "ftxtx: вв. начало имени ..-$fileDate.txt"  --inputbox  "часть содержания буфера:\n \n $tChk " 30 100 `
+name=`$DIALOG  \
+  --icon /usr/share/pixmaps/ftxtx.xpm \
+  --rc-file $gtkrcFile \
+  --cancel-label ">> ftxtxDir"  \
+  --stdout  \
+  --title "введите начало имени ..-$fileDate.txt"  \
+  --inputbox  "часть содержания буфера:\n \n $tChk " 30 100 \
+  `
+    case $? in
+          0)  
+          ;;
+          1)            
+            cmd=$(echo "${fm} ${ftxtxDir}") ;  
+            sh -c "${cmd}" 2> /tmp/ftxtx.err || 
+            $DIALOG \
+            --title "ftxtx:" \
+            --msgbox "$(cat /tmp/ftxtx.err)" 30 100 ; 
+            rm /tmp/ftxtx.err;  rm /tmp/ftxtx-$fileDate.txt ; 
+            exit 0
+          ;;      
+          255)
+    	    echo "закрытие окна." ;  rm /tmp/ftxtx-$fileDate.txt ; 
+    	    exit 0
+    	  ;; 
+    esac
 
-
-case $? in
-      0)
-      echo "Выбран ...." ;;
-    1)
-	echo "Отказ от ввода." ; rm /tmp/ftxtx-$fileDate.txt ; exit 0;; 
-    255)
-	echo "Нажата клавиша ESC." ;  rm /tmp/ftxtx-$fileDate.txt  ; exit 0;; 
- esac
-
-
-
-ftxtxDir=$(echo `awk -F 'ftxtxDir=' ' { print $2 }  ' $confFile`)
-
-[[ -d $ftxtxDir ]] || mkdir  $ftxtxDir
-
-
-
-####>> chk :
+####>> chk-prefix,w :
 [[  -z "$name" ]] && sffx=_$fileDate
 [[ -f $ftxtxDir/${name// /_}.txt ]] && sffx=_$fileDate
-
-
-
 
 ####>> rplc " " --> "_"
 file=$ftxtxDir/${name// /_}$sffx.txt
 
-
 ####>> +  name,path
 echo -e "${name}\n"'file://'"${file}\n""=========\n""=========\n""$(cat /tmp/ftxtx-$fileDate.txt)" > /tmp/ftxtx-$fileDate.txt
-
 
 ####>> + 3-empty
 echo -e "\n\n\n""=========\n""=========\n" >> /tmp/ftxtx-$fileDate.txt
 
-
-
 ## ## ## !!!
- mv /tmp/ftxtx-$fileDate.txt "${file}" 2> /tmp/ftxtx.err  
+mv /tmp/ftxtx-$fileDate.txt "${file}" 2> /tmp/ftxtx.err  
 
-[[ -z "$(cat /tmp/ftxtx.err)" ]] ||  ` Xdialog --title "ftxtx:" --msgbox "$(cat /tmp/ftxtx.err)" 30 100 ; exit 1  `
+## txtViewer :
+txtViewer=$(echo `awk -F 'txtViewer=' '{ print $2 }' $confFile`) 
 
-
-
-
-txtViewer=$(echo `awk -F 'txtViewer=' ' { print $2 }  ' $confFile`) 
-
-
-[[ -z "$(cat /tmp/ftxtx.err)" ]] &&  "${txtViewer}" "${file}" 
-
- rm /tmp/ftxtx.err
-
-
+## err:
+[[ -z "$(cat /tmp/ftxtx.err)" ]] || 
+  ` $DIALOG \
+  --title "ftxtx:" \
+  --msgbox "$(cat /tmp/ftxtx.err)" 30 100 ;
+  echo !!ОШИБКА-сохранения!_см.-/tmp/ftxtx-$fileDate.txt >> /tmp/ftxtx-$fileDate.txt 
+  sh -c `"${txtViewer}" /tmp/ftxtx-$fileDate.txt` ;
+  exit 1 \ 
+  `
+## ok:
+[[ -z "$(cat /tmp/ftxtx.err)" ]] && 
+  `cmd="${txtViewer}"" ""'"${file}"'" ; 
+  sh -c "${cmd}" 
+  `    
+rm /tmp/ftxtx.err
 
 exit 0
-
